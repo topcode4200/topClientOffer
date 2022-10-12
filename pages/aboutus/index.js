37385c65454c9a9ea1de3cdbc38fd5199b9cec2e
unknown <mehedi15-5815@diu.edu.bd>
2022-10-12 13:50:17 +0600

    added header footer

---
+++ pages/aboutus/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../../components/Footers/Footer';
+import Banner from './Banner';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <Banner/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default AboutUs;
